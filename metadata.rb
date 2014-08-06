f99707d7183834cd0f2f2fe1e8fe6494ac4e2735
Justin Roberts <justin.roberts@careerbuilder.com>
2014-08-06 17:40:21 -0400

    some more things

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Escape Studios'
 maintainer_email 'dev@escapestudios.com'
 license 'MIT'
 description 'Installs/Configures New Relic'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description 'yay'#IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.0.1'
 
 %w( debian ubuntu redhat centos fedora scientific amazon smartos ).each do |os|
